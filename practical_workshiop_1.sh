d4cc50507c55193a3f09f493d2f7e17183348874
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-03-23 12:55:28 +0000

    prctica Llaves

--- practical_workshiop_1.sh
+++ practical_workshiop_1.sh
@@ -3,9 +3,9 @@ which gpg
 gpg --version
 gpg --full-generate-key #generar llave
 gpg --list-keys
-gpg --armor --export 
+gpg --armor --export condorjeniffer0@gmail.com > mi_llave_publica.asc
 ls mi_llave_publica.asc
 gpg --armor --export 
 gpg --list-secret-keys --keyid-format=long #para listar las llaves privadas hash 
-#075C55ED21B1BD98
-gpg --armor --export-secret-keys #texto, no en binario 
+#C970546ABAFA2BE1 
+gpg --armor --export-secret-keys #para especifivar el hash de las llaves
