3b54ea7699587657ab26690f8570f873cab092cc
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-03-25 13:18:08 +0000

    filessistem

--- filessustem.sh
+++ filessustem.sh
@@ -1,2 +1,5 @@
 #comando
 pwd #print word directory -imprime la ruta
+cd / #Te mueve a la raíz del sistema de archivos.
+ls -F #muestra tipos de archivos con símbolos
+ls -i #muestra el número de inodo
