7c971b5422c905f95077e65f2ce9aaf0fb1b5534
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-03-25 13:45:50 +0000

    test.txt

--- filessustem.sh
+++ filessustem.sh
@@ -3,3 +3,5 @@ pwd #print word directory -imprime la ruta
 cd / #Te mueve a la raíz del sistema de archivos.
 ls -F #muestra tipos de archivos con símbolos
 ls -i #muestra el número de inodo
+echo "hola" >test.txt # crea el archivo con texto
+stat test.txt  # muestra información del archivo
