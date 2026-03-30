6ae9e0bb0d4c532c3f967f777a9045d029afb226
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-03-30 12:55:57 +0000

    install walls

---
+++ instalar pared.sh
@@ -0,0 +1,4 @@
+sudo apt update #Updates the list of available packages from the repositories. It doesn't install anything, it just refreshes the information.
+sudo apt upgrade #Install the available updates for the packages you already have installed.
+sudo apt install parted #Install the GNU Parted program, which is used to create, delete, and modify disk partitions.
+sudo parted -l && echo -e "/n---/n" && lsblk -f && echo -e"/n---/n" 
