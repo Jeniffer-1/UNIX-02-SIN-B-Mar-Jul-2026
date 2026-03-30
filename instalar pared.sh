5923e00b7713339e037ab7354241c75b405b516a
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-03-30 13:50:13 +0000

    create and display file information

--- instalar pared.sh
+++ instalar pared.sh
@@ -3,3 +3,5 @@ sudo apt upgrade #Install the available updates for the packages you already hav
 sudo apt install parted #Install the GNU Parted program, which is used to create, delete, and modify disk partitions.
 sudo parted -l && echo -e "/n---/n" && lsblk -f && echo -e"/n---/n" #It's a command to check how your disks are organized in Linux.
 [ -d/sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if your system booted in UEFI or BIOS mode.
+echo "esto es un archivo" > archivo.txt #Create the file file.txt
+stat archivo.txt #Displays file information
