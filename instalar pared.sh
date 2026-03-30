d1a0f7d45c19c388a2121a4d6b31a569732ed54b
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-03-30 13:13:08 +0000

    Check, UEFI or BIOS.

--- instalar pared.sh
+++ instalar pared.sh
@@ -2,3 +2,4 @@ sudo apt update #Updates the list of available packages from the repositories. I
 sudo apt upgrade #Install the available updates for the packages you already have installed.
 sudo apt install parted #Install the GNU Parted program, which is used to create, delete, and modify disk partitions.
 sudo parted -l && echo -e "/n---/n" && lsblk -f && echo -e"/n---/n" #It's a command to check how your disks are organized in Linux.
+[ -d/sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if your system booted in UEFI or BIOS mode.
