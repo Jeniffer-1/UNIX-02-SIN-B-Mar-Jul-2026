a002708e8dca1021c98bbc95bb93609bf577c922
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-04-06 12:32:57 +0000

    change directories to directories

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+cd / #It takes you to the root of the system
+cd /home/codespace #It moves you to a specific route
+cd ~ #It takes you to your personal directory.
+cd $HOME #It does the same thing as a cd ~
