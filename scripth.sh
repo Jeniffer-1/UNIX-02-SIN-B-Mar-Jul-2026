9839b32d808eb37d4386be4b3594624b4e6abb9a
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-04-13 12:41:07 +0000

    comands ls

---
+++ scripth.sh
@@ -0,0 +1,13 @@
+ls -a #short option
+ls --all #lot option
+ls -a / #Displays all files and folders in the root directory, including hidden ones.
+#comands, option , arguments
+#comand (ls)
+#short (-a)
+#long (--all) 
+#is the root directory the main system folder (/)
+ls -l
+ls -a 
+ls -l -a -h
+ls -l -ah
+ls -lah
