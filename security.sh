dcad6764e17baab8854cb18cc92221c7bad175e1
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-05-04 13:40:00 +0000

    group users

--- security.sh
+++ security.sh
@@ -17,3 +17,99 @@ cat /etc/passwd | head -10 #It shows the first 10 users of the system.
 cat /etc/group | head -10 #Displays the first 10 groups in the system (from the file where they are saved).
 groups #Displays the groups to which the current user belongs.
 groups $USER #Displays the user groups you specify (in this case, your current user using the $USER variable).
+id -u
+id -g
+id -G
+cat /etc/group
+#result:
+#root:x:0:
+#daemon:x:1:
+#bin:x:2:
+#sys:x:3:
+#adm:x:4:
+#tty:x:5:
+#disk:x:6:
+#lp:x:7:
+#mail:x:8:
+#news:x:9:
+#uucp:x:10:
+#man:x:12:
+#proxy:x:13:
+#kmem:x:15:
+#dialout:x:20:
+#fax:x:21:
+#voice:x:22:
+#cdrom:x:24:
+#floppy:x:25:
+#tape:x:26:
+#sudo:x:27:
+#audio:x:29:
+#dip:x:30:
+#www-data:x:33:
+#backup:x:34:
+#operator:x:37:
+#list:x:38:
+#irc:x:39:
+#src:x:40:
+#shadow:x:42:
+#utmp:x:43:
+#video:x:44:
+#sasl:x:45:
+#plugdev:x:46:
+#staff:x:50:
+#games:x:60:
+#users:x:100:
+#nogroup:x:65534:
+#lpadmin:x:999:
+#systemd-journal:x:998:
+#systemd-network:x:997:
+#debian-tor:x:101:
+#tcpdump:x:996:
+cat /etc/group | grep users
+#result:
+#users:x:100:
+cat /etc/gshadow
+#result:
+#root:*::
+#daemon:*::
+#bin:*::
+#sys:*::
+#adm:*::
+#tty:*::
+#disk:*::
+#lp:*::
+#mail:*::
+#news:*::
+#uucp:*::
+#man:*::
+#proxy:*::
+#kmem:*::
+#dialout:*::
+#fax:*::
+#voice:*::
+#cdrom:*::
+#floppy:*::
+#tape:*::
+#sudo:*::
+#audio:*::
+#dip:*::
+#www-data:*::
+#backup:*::
+#operator:*::
+#list:*::
+#irc:*::
+#src:*::
+#shadow:*::
+utmp:*::
+video:*::
+sasl:*::
+plugdev:*::
+staff:*::
+games:*::
+users:*::
+nogroup:*::
+lpadmin:!*::
+systemd-journal:!*::
+systemd-network:!*::
+debian-tor:!::
+tcpdump:!*::
