800de94d89bebd3049594c66904066b494fc7319
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-05-27 13:36:51 +0000

    newgrp

--- Security2.sh
+++ Security2.sh
@@ -25,3 +25,29 @@ id -gn
 echo "nuevo grupo activo:$(id -gn)"
 #nuevo grupo activo:desarrolladores
                                     
+#create file in the subshell
+touch ~/dentro_de_newgrp.txt
+ls -la ~/dentro_de_newgrp.txt 
+#-rw-r--r-- 1 root desarrolladores 0 may 27 13:32 /root/dentro_de_newgrp.txt    
+#crate directori
+mkdir -p ~/proyecto_dev/scr
+ls -la ~/
+#total 64
+#drwx------ 1 root root             4096 may 27 13:33 .
+#drwxr-xr-x 1 root root             4096 may 27 12:38 ..
+#-rw-r--r-- 1 root root                0 may 27 13:06 antes_de_newgrp.txt
+#-rw-r--r-- 1 root root             5578 may 24 04:01 .bashrc
+#-rw-r--r-- 1 root root              607 may 24 04:01 .bashrc.original
+#drwxr-xr-x 3 root root             4096 may 27 12:38 .cache
+#drwx------ 3 root root             4096 may 27 12:38 .config
+#-rw-r--r-- 1 root desarrolladores     0 may 27 13:32 dentro_de_newgrp.txt
+#lrwxrwxrwx 1 root root               47 may 27 12:38 .docker -> /workspaces/.codespaces/.persistedshare/.docker
+#drwxr-xr-x 3 root root             4096 may 27 12:38 .dotnet
+#drwx------ 4 root root             4096 may 27 12:38 .local
+#-rw-r--r-- 1 root root              132 may 15 11:37 .profile
+#drwxr-xr-x 3 root desarrolladores  4096 may 27 13:33 proyecto_dev
+#drwx------ 2 root root             4096 may 27 12:37 .ssh
+#-rw-r--r-- 1 root root                0 may 27 12:59 test_gupo_heredado.txt
+#drwxr-xr-x 5 root root             4096 may 27 12:38 .vscode-remote
+#-rw------- 1 root root                0 may 27 12:39 .zsh_history
+#-rw-r--r-- 1 root root            10975 may 27 12:38 .zshrc
