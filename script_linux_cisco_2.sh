cf8de390cfa4ab4f70b186cb89e3ac908d58ef6e
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-05-31 00:14:44 +0000

    ls documents

---
+++ script_linux_cisco_2.sh
@@ -0,0 +1,12 @@
+sysadmin@localhost:~$ ls #means list. It displays the files and folders in the current directory.       
+#result                                                                      
+#Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos       
+sysadmin@localhost:~$ LS #means list. It displays the files and folders in the current directory.
+#result                                                      
+#-bash: /usr/games/LS: Permission denied  
+sysadmin@localhost:~$ ls Documents #This command lists all files and folders inside the Documents directory.
+#result                                           
+#School           alpha-second.txt  food.txt     linux.txt     os.csv            
+#Work             alpha-third.txt   hello.sh     longfile.txt  people.csv        
+#adjectives.txt   alpha.txt         hidden.txt   newhome.txt   profile.txt       
+#alpha-first.txt  animals.txt       letters.txt  numbers.txt   red.txt  
