02c55035d2cccf96170f30bf7810896661ef8180
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-01 13:12:15 +0000

    -r -n -x

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -9,4 +9,6 @@ echo ${OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
-bash -r blackhatbash1.sh #ejecuatar en modo restringido - run in restricted mode
+#bash -r blackhatbash1.sh #ejecuatar en modo restringido - run in restricted mode
+#bash -n blackhatbash1.sh #muestra errores de sintaxis  - shows syntax errors -n #debugging 
+#bash -x blackhatbash1.sh #activa modo verboso. active verbose mode
