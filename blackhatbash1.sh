8b5c062cc77cedcebab66489516bb85a96fbbe22
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-01 13:18:58 +0000

    set +x -x

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #!/bin/bash -x #muestra los comandos en timpo real- displays commands in real time
+set -x #enlables trace mode
 bash --version
 env
 echo ${SHELL}
@@ -12,3 +13,5 @@ df --human-readable
 #bash -r blackhatbash1.sh #ejecuatar en modo restringido - run in restricted mode
 #bash -n blackhatbash1.sh #muestra errores de sintaxis  - shows syntax errors -n #debugging 
 #bash -x blackhatbash1.sh #activa modo verboso. active verbose mode
+
+set +x #disable trace mode
