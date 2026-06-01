7556eb3a5e46682cb9c25db51a3ceda011d3d2db
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-01 13:44:04 +0000

    black hat bash

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -4,3 +4,11 @@ book="black hat bash"
 echo "This book's name is ${book}"
 echo "this book's name is $book"
 set +x
+
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+#result
+#drwxr-xr-x 1 root root 4096 jun  1 12:40 /
+book="black hat bash"
+unset book
+echo "${book}"
