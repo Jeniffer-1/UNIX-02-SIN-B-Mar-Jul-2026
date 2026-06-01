d6b8ab001bf5ef809d4be9db1ee7ee8fc4630d71
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-01 00:21:57 +0000

    memori RAM

--- script_linux_cisco_2.sh
+++ script_linux_cisco_2.sh
@@ -1,12 +1,8 @@
-sysadmin@localhost:~$ ls #means list. It displays the files and folders in the current directory.       
-#result                                                                      
-#Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos       
-sysadmin@localhost:~$ LS #means list. It displays the files and folders in the current directory.
-#result                                                      
-#-bash: /usr/games/LS: Permission denied  
-sysadmin@localhost:~$ ls Documents #This command lists all files and folders inside the Documents directory.
-#result                                           
-#School           alpha-second.txt  food.txt     linux.txt     os.csv            
-#Work             alpha-third.txt   hello.sh     longfile.txt  people.csv        
-#adjectives.txt   alpha.txt         hidden.txt   newhome.txt   profile.txt       
-#alpha-first.txt  animals.txt       letters.txt  numbers.txt   red.txt  
+sysadmin@localhost:~$ dd if=/dev/zero of=/tmp/swapex bs=1M count=50      
+#It is commonly used to create a fixed-size file that can later be configured as swap space.
+# Swap space helps the operating system when RAM is running low by using disk space as additional memory.  
+#result     
+#50+0 records in                                                                 
+#50+0 records out                                                                
+#52428800 bytes (52 MB) copied, 0.028 s, 1.9 GB/s 
+
