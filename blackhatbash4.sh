5fd3d21f9439f82271636c9ae6ebf8edfd3ab78b
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-03 12:28:05 +0000

    result blackhatbash4

--- blackhatbash4.sh
+++ blackhatbash4.sh
@@ -2,6 +2,18 @@
 set -x
 let result="4 * 5"
 echo ${result}
+#result
+#+ set -x
+#+ let 'result=4 * 5'
+#+ echo 20
+#20
 
-20 
+result=$((5*5))
+echo ${result}
+#result
+#+ result=25
+#+ echo 25
+#25
+#+ set +x
 set +x
+
