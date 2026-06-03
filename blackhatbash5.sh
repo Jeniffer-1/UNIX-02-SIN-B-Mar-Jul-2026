2eabe42477101b3b2634fabc3e3d7ad9d7a800b9
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-03 13:39:09 +0000

    commit cat stderr

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -6,3 +6,16 @@ ls; ps; whoami
 lzl || echo "el comando fallo"
 ls -l / &> stdout_and_stderr.txt
 ls -l / 1> stdout.txt 2> stderr.txt
+echo "uno" > archivo.txt
+cat archivo.txt
+echo "uno-uno" >archivo.txt
+cat archivo.txt
+echo "dos -dos" > archivo.txt
+cat archivo.txt
+echo "uno-uno" >> archivo.txt
+echo "dos -dos" >> archivo.txt
+ cat archivo.txt
+cat << EOF
+Black Hat Bash
+by No Starch Press
+EOF
