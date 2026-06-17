7070207116abc693d8a49fb88a61f5fefbc7614f
Jeniffer-1 <jecondorpi@uide.edu.ec>
2026-06-17 13:01:12 +0000

    commit-new log and log

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 awk '{print $1,$2,$3}' log.txt
+
 awk '{print $1}' log.txt
 awk '{print $2}' log.txt
 awk '{print $3}' log.txt
@@ -10,3 +11,11 @@ awk 'NR < 10' log.txt
 #show 10 lineas 
 
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
+
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+sed - i 's/Mozilla/Godzilla/g' log.txt 
+grep "Godzilla" log.txt
+
+sed 's/ //g' log.txt
